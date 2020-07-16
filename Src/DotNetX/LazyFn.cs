6de0922431c04d3f011d142cb82a8f64863d88cf
Iskander Sierra <iskander.sierra@gmail.com>
2020-07-16 10:39:07 +0200

    Add LazyFn and PropertyEqualityComparer

---
+++ Src/DotNetX/LazyFn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace DotNetX
+{
+    public static class LazyFn
+    {
+        public static Lazy<T> Create<T>(Func<T> valueFactory) =>
+            new Lazy<T>(valueFactory);
+
+        public static Lazy<T> Create<T>(Func<T> valueFactory, bool isThreadSafe) =>
+            new Lazy<T>(valueFactory, isThreadSafe);
+
+        public static Lazy<T> Create<T>(Func<T> valueFactory, LazyThreadSafetyMode mode) =>
+            new Lazy<T>(valueFactory, mode);
+    }
+}
---
+++ Src/DotNetX/PropertyEqualityComparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DotNetX
+{
+    public class PropertyEqualityComparer<T, P> : IEqualityComparer<T>, IEqualityComparer
+    {
+        readonly Func<T, P> getProperty;
+        readonly IEqualityComparer<P> propertyComparer;
+
+        public PropertyEqualityComparer(Func<T, P> getProperty, IEqualityComparer<P>? propertyComparer = null)
+        {
+            this.getProperty = getProperty ?? throw new ArgumentNullException(nameof(getProperty));
+            this.propertyComparer = propertyComparer ?? EqualityComparer<P>.Default;
+        }
+
+        public bool Equals(T x, T y) =>
+            propertyComparer.Equals(getProperty(x), getProperty(y));
+
+        public int GetHashCode(T obj) =>
+            propertyComparer.GetHashCode(getProperty(obj));
+
+        bool IEqualityComparer.Equals(object x, object y) =>
+            x is T a && y is T b ? Equals(a, b) : Object.Equals(x, y);
+
+        int IEqualityComparer.GetHashCode(object obj) =>
+            obj is T item ? GetHashCode(item) : (obj == null ? 0 : obj.GetHashCode());
+    }
+}
