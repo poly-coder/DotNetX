e87cabe45d40f88cea7c1b6af8ba44858846f6ee
Iskander Sierra <iskander.sierra@gmail.com>
2021-09-05 13:30:28 +0200

    v0.1.16 Extract interface for ReliableAsyncService Add MailboxActor, similar to F# MailboxProcessor, but using Channels as inbox

---
+++ Src/DotNetX/DelegateReliableAsyncService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetX
+{
+    public class DelegateReliableAsyncService<TService> :
+        ReliableAsyncService<TService>
+    {
+        private readonly Func<CancellationToken, Task<TService>> createService;
+        private readonly Func<Exception, bool> isInvalidatingError;
+
+        public DelegateReliableAsyncService(
+            Func<CancellationToken, Task<TService>> createService,
+            Func<Exception, bool>? isInvalidatingError = null)
+        {
+            this.createService = createService ?? throw new ArgumentNullException(nameof(createService));
+            this.isInvalidatingError = isInvalidatingError ?? (ex => base.IsInvalidatingError(ex));
+        }
+
+        protected override Task<TService> CreateService(CancellationToken cancellationToken) =>
+            createService(cancellationToken);
+
+        public override bool IsInvalidatingError(Exception exception)
+        {
+            return isInvalidatingError(exception);
+        }
+    }
+}
---
+++ Src/DotNetX/IReliableAsyncService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetX
+{
+    public interface IReliableAsyncService<TService>
+    {
+        Task<TService> GetServiceAsync(CancellationToken cancellationToken = default);
+        
+        void Invalidate();
+
+        Task InvalidateAsync(CancellationToken cancellationToken = default);
+
+        bool IsInvalidatingError(Exception exception);
+
+        public async Task<T> WithService<T>(
+            Func<TService, Task<T>> action,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var service = await GetServiceAsync(cancellationToken);
+
+                return await action(service);
+            }
+            catch (Exception exception)
+            {
+                if (IsInvalidatingError(exception))
+                {
+                    await InvalidateAsync(cancellationToken);
+                }
+
+                throw;
+            }
+        }
+
+        public async Task WithService(
+            Func<TService, Task> action,
+            CancellationToken cancellationToken = default)
+        {
+            await WithService(
+                async container =>
+                {
+                    await action(container);
+                    return true;
+                },
+                cancellationToken);
+        }
+    }
+}
